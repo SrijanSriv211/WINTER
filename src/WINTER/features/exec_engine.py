8ee9ed41eeb484395d56db76fc207f052c7e8725
SrijanSriv211 <QCoreNest@gmail.com>
2024-05-29 13:19:47 +0530

    Rename features.py to exec_engine.py

--- src/WINTER/features/exec_engine.py
+++ src/WINTER/features/exec_engine.py
@@ -2,20 +2,26 @@ from ..shared.utils import get_bin_path, dprint
 from . import func
 import random, json, os
 
-class Features:
+class ExecEngine:
     def __init__(self, filepath):
         self.filepath = filepath
+
+        self.AOs_startup_path = os.path.join(get_bin_path(), "AOs\\Files.x72\\etc\\Startup\\exec_engine.aos")
+        with open(self.AOs_startup_path, "w", encoding="utf-8") as f:
+            f.write(".")
 
         with open(self.filepath, "r", encoding="utf-8") as f:
             self.jsondata = json.load(f)
         self.func_dict = {}
+
+    def loadAOs(self):
+        print(f"start \"{os.path.join(get_bin_path(), 'AOs\\AOs.exe')}\"")
 
     # Map all the functions corresponding to their respective skills.
     def load(self):
         self.func_dict = {
             "play,video": func.play_video,
             "play,music": func.play_music,
-            "open,game": func.playgames,
             "play,on_youtube": func.youtube,
             "search,wikipedia": func.search_on_wikipedia,
             "translate,one_lang_to_another": func.translate
@@ -82,6 +88,7 @@ class Features:
             args = task["args"]
             exec_engine = task["execution_engine"]
             extraction_models = task["extraction_models"]
+
             print(skillname, cmd, args, exec_engine)
             if exec_engine == None:
                 break
@@ -90,14 +97,22 @@ class Features:
                 # evaluate the model to extract text from 'input_prompt'
                 # replace that extracted text with the integer value in args.
 
-                # evaluate model here.
-                pass
+                extracted_texts = []
+                for i, x in enumerate(args):
+                    if isinstance(x, int):
+                        args[i] = ""
 
             if exec_engine == "func" and skillname in self.func_dict.keys():
-                self.func_dict[skillname](*args)
+                # self.func_dict[skillname](*args)
+                pass
 
             elif exec_engine == "AOs":
-                pass
+                with open(self.AOs_startup_path, "r", encoding="utf-8") as f:
+                    lines = f.readlines()
+                    lines[-1] = f"{cmd} {' '.join(args)}" + "\n."
+
+                with open(self.AOs_startup_path, "w", encoding="utf-8") as f:
+                    f.writelines(lines)
 
             elif exec_engine == "skills":
                 for intent in self.jsondata["skills"]:
@@ -106,11 +121,3 @@ class Features:
                         break
 
                 self.__exec_tasks__(input_prompt, tasks, skillname)
-
-            elif exec_engine == "external":
-                # Look into the 'bin\powertoys' folder
-                # 'cmd' will be the app/script name that 'bin\powertoys' folder,
-                # 'args' will the arguments that will be passed in that app/script.
-                powertoys_path = os.path.join(get_bin_path(), "powertoys")
-                for file in os.listdir(powertoys_path):
-                    os.system(f"{os.path.join(powertoys_path, file)} {' '.join(args)}")
