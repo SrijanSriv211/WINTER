213a2966e02bdbd19f84b8a8c95a6f6c20b761b9
SrijanSriv211 <QCoreNest@gmail.com>
2024-08-04 15:02:09 +0530

    Create GPT, LSTM & RNN models

---
+++ src/models/lstm/model.py
@@ -0,0 +1,57 @@
+from torch.nn import functional as F
+import torch.nn as nn, torch
+
+class LSTMConfig:
+    n_embd = 2
+    n_hidden = 2
+    n_layer = 1
+    block_size = 16 # what is the maximum context length for predictions?
+    input_size = None
+    device = None
+
+# https://github.com/SrijanSriv211/GAT-w/blob/52fb27a0da4da516400316c016d08d9317142fb2/main.py
+class LSTM(nn.Module):
+    def __init__(self):
+        super(LSTM, self).__init__()
+
+        # Defining the layers
+        self.embedding = nn.Embedding(LSTMConfig.input_size, LSTMConfig.n_hidden)
+        self.lstm = nn.LSTM(LSTMConfig.n_hidden, LSTMConfig.n_hidden, LSTMConfig.n_layer, batch_first=True)
+        self.fc = nn.Linear(LSTMConfig.n_hidden, LSTMConfig.input_size) # Fully connected layer
+
+    def forward(self, x, targets=None):
+        batch_size = x.size(0)
+
+        # Pass input through the embedding layer
+        x = self.embedding(x)
+
+        # Initialize hidden state for the first input using method defined below
+        hidden = self.init_hidden(batch_size)
+
+        # Passing in the input and hidden state into the model and obtaining outputs
+        out, hidden = self.lstm(x, hidden)
+
+        # Reshaping the outputs such that it can be fit into the fully connected layer
+        out = out.contiguous().view(-1, LSTMConfig.n_hidden)
+        out = self.fc(out)
+
+        # Calculate loss
+        loss = None if targets is None else F.cross_entropy(out, targets)
+
+        return out, loss
+
+    def init_hidden(self, batch_size):
+        # Initialize both hidden state and cell state
+        hidden = (torch.zeros(LSTMConfig.n_layer, batch_size, LSTMConfig.n_hidden).to(LSTMConfig.device), torch.zeros(LSTMConfig.n_layer, batch_size, LSTMConfig.n_hidden).to(LSTMConfig.device))
+        return hidden
+    
+    def predict(self, x, temperature=1.0):
+        out, _ = self(x)
+
+        # Adjust the output probabilities with temperature
+        prob = nn.functional.softmax(out[-1] / temperature, dim=0).data
+
+        # Sample from the modified distribution
+        idx = torch.multinomial(prob, 1).item()
+
+        return idx
---
+++ src/models/rnn/model.py
@@ -0,0 +1,38 @@
+from torch.nn import functional as F
+import torch.nn as nn, torch
+
+class RNNConfig:
+    n_hidden = 2
+    n_layer = 1
+    input_size = None
+    output_size = None
+    device = None
+
+class RNN(nn.Module):
+    def __init__(self):
+        super(RNN, self).__init__()
+
+        # Defining the layers
+        self.rnn = nn.RNN(RNNConfig.input_size, RNNConfig.n_hidden, RNNConfig.n_layer, batch_first=True) # RNN Layer
+        self.fc = nn.Linear(RNNConfig.n_hidden, RNNConfig.output_size) # Fully connected layer
+
+    def forward(self, x, targets=None):
+        # Apply RNN layer
+        out, _ = self.rnn(x)
+
+        # Squeeze the dimensions to remove the batch_first dimension
+        out = self.fc(out)
+
+        # Calculate loss
+        loss = None if targets is None else F.cross_entropy(out, targets)
+        return out, loss
+
+    def predict(self, x):
+        out, _ = self(x)
+        _, predicted = torch.max(out, dim=1)
+
+        probs = torch.softmax(out, dim=1)
+        prob = probs[0][predicted.item()]
+        confidence = prob.item()
+
+        return predicted.item(), confidence
