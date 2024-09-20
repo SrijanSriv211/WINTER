770c6ab077c265fbb56a7c732ab9d46ea7e7cb69
SrijanSriv211 <QCoreNest@gmail.com>
2024-09-20 15:10:14 +0530

    Demo train code with the new GPT trainer

--- GATw.py
+++ GATw.py
@@ -1,67 +1,59 @@
-from src.models.gpt import train, model as gpt
+from src.models.gpt import train, sample
 from src.tokenizers.bpe.regex import RegexTokenizer
 from src.shared.utils import dprint
-import torch, json, os
-
-# raw_data = []
-
-# x = os.listdir("data")
-# x.remove("clis.json")
-# x.remove("lo.json")
-# x.remove("data.txt")
-# x.remove("LLM data.txt")
-# x.remove("conversations.txt")
-
-# with open("data\\clis.json", "r", encoding="utf-8") as f:
-#     obj = json.load(f)
-
-# for intent in obj["clis"]:
-#     if intent["skill"] == "default" or intent["patterns"] == [""] or intent["patterns"] == []:
-#         continue
-
-#     raw_data.extend(intent["patterns"])
-
-# for i in x:
-#     with open(f"data\\{i}", "r", encoding="utf-8") as f:
-#         raw_data.append(f.read())
-
-# with open(f"data\\data.txt", "r", encoding="utf-8") as f:
-#     t = f.read()
-#     i = int(len(t) * 0.02) + 49000 - 250 - 171
-#     raw_data.append(t[:i])
-
-# with open(f"data\\LLM data.txt", "r", encoding="utf-8") as f:
-#     t = f.read()
-#     i = int(len(t) * 0.1) + 400000 + 170
-#     raw_data.append(t[:i])
-
-# with open(f"data\\conversations.txt", "r", encoding="utf-8") as f:
-#     t = f.read()
-#     i = int(len(t) * 0.01) - 50000 + 114
-#     raw_data.append(t[:i])
+import torch
 
 tokenizer = RegexTokenizer()
 tokenizer.load("bin\\models\\cl2k.model")
-# tokenizer.train("\n".join(raw_data), 2279, verbose=True)
+# tokenizer.train(raw_data, 2279, verbose=True)
 tokenizer.register_special_tokens({"<|clis|>": 2279, "<|startoftext|>": 2280, "<|endoftext|>": 2281})
 # tokenizer.save("bin\\models\\cl2k")
 
-gpt.GPTConfig.vocab_size = 2482 # tokenizer vocab size + number of special tokens
-gpt.GPTConfig.block_size = 50
-gpt.GPTConfig.n_layer = 2
-gpt.GPTConfig.n_head = 2
-gpt.GPTConfig.n_embd = 32
-gpt.GPTConfig.device = "cpu"
+CONFIG = dict(
+    # checkpoints
+    checkpoints = None,
+
+    # data
+    gradient_accumulation_steps = 1, # used to simulate larger batch sizes
+    batch_size = 32, # if gradient_accumulation_steps > 1, this is the micro-batch size
+    block_size = 50,
+
+    # model
+    vocab_size = 2482,
+    n_layer = 4,
+    n_head = 4,
+    n_embd = 32,
+    dropout = 0.0, # for pretraining 0 is good, for finetuning try 0.1+
+    bias = False, # do we use bias inside LayerNorm and Linear layers?
+
+    # adamw optimizer
+    learning_rate = 4e-3, # max learning rate
+    weight_decay = 1e-1,
+    beta1 = 0.9,
+    beta2 = 0.95,
+    grad_clip = 1, # clip gradients at this value, or disable if == 0.0
+
+    # learning rate decay settings
+    decay_lr = True, # whether to decay the learning rate
+    warmup_iters = 1000, # how many steps to warm up for
+    lr_decay_iters = 10000, # should be ~= max_iters per Chinchilla
+    min_lr = 1e-4, # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
+
+    # system
+    device = "cpu", # examples: "cpu", "cuda", "cuda:0", "cuda:1" etc., or try "mps" on macbooks
+    seed = "auto", # examples: "auto", 1337 or any other number
+    compile = False # use PyTorch 2.0 to compile the model to be faster
+)
 
 with open("data\\claw.txt", "r", encoding="utf-8") as f:
     data = tokenizer.encode(f.read(), allowed_special="all")
 
-# out = torch.load("bin\\models\\claw.pth")
-# t = train.train(32, out)
-t = train.train(32)
-t.prepare(data, 1)
-out = t.train(4e-3, 1000)
-t.save(out, "bin\\models\\claw")
+out = torch.load("bin\\models\\claw.pth")
+t = train.train(CONFIG)
+t.from_scratch()
+t.prepare_data(data, 1)
+out = t.train(max_iters=10000, eval_interval=1000)
+torch.save(out, "bin\\models\\claw.pth")
 
-i = gpt.inference(out, True)
+i = sample.sample(out, auto_load=True)
 dprint(tokenizer.decode(i.generate()))
