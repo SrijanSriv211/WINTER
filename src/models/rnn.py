052362a44f43a911745d7cfb6ddaf1ed19dcd8fa
SrijanSriv211 <QCoreNest@gmail.com>
2024-11-04 23:26:34 +0530

    Create a seperate RNN trainer

--- src/models/rnn.py
+++ src/models/rnn.py
@@ -1,23 +1,11 @@
-from ..shared.utils import calc_total_time
 from colorama import Style, Fore, init
+from dataclasses import dataclass
 from torch.nn import functional as F
-import matplotlib.pyplot as plt
-import torch.nn as nn, torch
-import time, os
+import torch.nn as nn, torch.amp, torch, inspect
 
 init(autoreset=True)
 
-# ---------------------------------------------------------------------------
-# ---------------------------------------------------------------------------
-#                       __  __  ____  _____  ______ _      
-#                      |  \/  |/ __ \|  __ \|  ____| |     
-#                      | \  / | |  | | |  | | |__  | |     
-#                      | |\/| | |  | | |  | |  __| | |     
-#                      | |  | | |__| | |__| | |____| |____ 
-#                      |_|  |_|\____/|_____/|______|______|
-# ---------------------------------------------------------------------------
-# ---------------------------------------------------------------------------
-
+@dataclass
 class RNNConfig:
     input_size: int = None
     output_size: int = None
@@ -36,7 +24,7 @@ class RNN(nn.Module):
 
         # Defining the layers
         self.embedding = nn.Embedding(config.input_size, config.n_embd) if config.n_embd != None else None # Embedding Layer
-        self.rnn = nn.RNN(config.n_embd, config.n_hidden, config.n_layer, bias=config.bias, batch_first=True, dropout=config.dropout) # RNN Layer
+        self.rnn = nn.RNN(config.n_embd, config.n_hidden, config.n_layer, bias=True, batch_first=True, dropout=config.dropout) # RNN Layer
         self.fc = nn.Linear(config.n_hidden, config.output_size) # Fully connected layer
 
     def forward(self, x, targets=None):
@@ -53,6 +41,15 @@ class RNN(nn.Module):
         # Calculate loss
         loss = None if targets is None else F.cross_entropy(out, targets)
         return out, loss
+
+    def configure_optimizers(self, learning_rate, device_type):
+        # Create AdamW optimizer and use the fused version if it is available
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and device_type == 'cuda'
+        color = f"{Fore.LIGHTGREEN_EX}{Style.BRIGHT}" if use_fused == True else f"{Fore.LIGHTRED_EX}{Style.BRIGHT}"
+        print(f"Using fused AdamW: {color}{use_fused}")
+        optimizer = torch.optim.AdamW(self.parameters(), lr=learning_rate, betas=(0.9, 0.95), fused=use_fused)
+        return optimizer
 
     def predict(self, x):
         out, _ = self(x)
@@ -104,236 +101,3 @@ class sample:
     def prepare_context(self, X):
         X = X.reshape(1, X.shape[0])
         return torch.tensor(X).to(RNNConfig.device)
-
-# ---------------------------------------------------------------------------
-# ---------------------------------------------------------------------------
-#                   _______ _____            _____ _   _ 
-#                  |__   __|  __ \     /\   |_   _| \ | |
-#                     | |  | |__) |   /  \    | | |  \| |
-#                     | |  |  _  /   / /\ \   | | | . ` |
-#                     | |  | | \ \  / ____ \ _| |_| |\  |
-#                     |_|  |_|  \_\/_/    \_\_____|_| \_|
-# ---------------------------------------------------------------------------
-# ---------------------------------------------------------------------------
-
-class train:
-    def __init__(self, config):
-        """
-        `config`: configuration for how to setup the trainer
-        ```python
-        dict(
-            # checkpoints
-            checkpoints = {
-                "path": "directory-to-save-in",
-                "name": "model-name",
-                "interval": after-how-many-iters-to-save-checkpoint(int)
-            },
-
-            # data
-            batch_size = 32,
-
-            # model
-            input_size = 2482,
-            output_size = 2482,
-            n_layer = 4,
-            n_hidden = 4,
-            n_embd = 32,
-            dropout = 0.0, # for pretraining 0 is good, for finetuning try 0.1+
-            bias = False, # do we use bias inside LayerNorm and Linear layers?
-
-            # adamw optimizer
-            learning_rate = 4e-2, # max learning rate
-
-            # system
-            device = "cpu", # examples: "cpu", "cuda", "cuda:0", "cuda:1" etc., or try "mps" on macbooks
-            seed = "auto", # examples: "auto", 1337 or any other number
-        )
-        ```
-        """
-
-        self.config = config
-        self.device = ("cuda" if torch.cuda.is_available() else "cpu") if self.config["device"] == "auto" else self.config["device"]
-
-        torch.manual_seed(self.config["seed"]) if self.config["seed"] != "auto" else None
-
-        # print the device
-        print("Training on", f"{Fore.YELLOW}{Style.BRIGHT}{self.device}", f"{Fore.WHITE}{Style.BRIGHT}({torch.initial_seed()})")
-
-    def from_scratch(self):
-        self.hyperparams = dict(dropout=self.config["dropout"])
-        # read off the created config params, so we can store them into checkpoint correctly
-        for k in ["n_layer", "n_head", "n_embd", "output_size", "bias", "input_size"]:
-            self.hyperparams[k] = self.config[k]
-
-        rnnconf = RNNConfig(**self.hyperparams)
-        # create an instance of RNN
-        self.model = RNN(rnnconf)
-        self.model.to(self.device)
-
-        # a dict for keep track of all the losses to be plotted.
-        self.metrics = {
-            "train": [],
-            "eval": [],
-            "val": []
-        }
-        self.iter_num = 0
-        self.best_loss = 0
-
-    def prepare(self, encoded_data, data_division=1):
-        """
-        `data`: The encoded training text data.
-
-        For eg,
-        ```python
-        encode(text, stoi=self.stoi)
-        ```
-        """
-
-        data = torch.tensor(encoded_data, dtype=torch.long)
-
-        # train and test splits
-        n = int(data_division * len(data)) # the first (data_division * 100)% will be train, rest val
-        self.train_data = data[:n]
-        self.val_data = data[n:] if 0 < data_division < 1 else data[:n]
-
-        # print the number of tokens
-        print(f"{Fore.WHITE}{Style.BRIGHT}{(len(data)/1e6)}M", "total tokens")
-        print(
-            f"{Fore.WHITE}{Style.BRIGHT}{(len(self.train_data)/1e6)}M", "train tokens,",
-            f"{Fore.WHITE}{Style.BRIGHT}{(len(self.val_data)/1e6)}M", "test tokens",
-            f"   {Fore.WHITE}{Style.DIM}(Using train tokens as test tokens)" if not (0 < data_division < 1) else ""
-        )
-
-    # data loading
-    def get_batch(self, split):
-        # generate a small batch of data of inputs x and targets y
-        data = self.train_data if split == "train" else self.val_data
-        ix = torch.randint(len(data) - 1, (self.config["batch_size"],))
-        x = torch.stack([torch.tensor(data[i][0]) for i in ix])
-        y = torch.stack([torch.tensor(data[i][1]) for i in ix])
-        x, y = x.to(self.device), y.to(self.device)
-        return x, y
-
-    @torch.no_grad()
-    def estimate_loss(self, eval_iters):
-        out = []
-        self.model.eval()
-        for split in ["train", "val"]:
-            losses = torch.zeros(eval_iters)
-            for k in range(eval_iters):
-                X, Y = self.get_batch(split)
-                _, loss = self.model(X, Y)
-
-                losses[k] = loss.item()
-            out[split] = losses.mean()
-        self.model.train()
-        return out
-
-    def train(self, max_iters=1000, eval_interval=100, log_interval=100, eval_iters=100, patience=10):
-        # report number of parameters
-        n_params = sum(p.numel() for p in self.model.parameters())
-        print(f"{Fore.WHITE}{Style.BRIGHT}{n_params/1e6}M", "parameters")
-
-        # create a PyTorch optimizer
-        optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.config["learning_rate"])
-
-        # training loop
-        start_time = time.time()
-        eval_t0 = time.time()
-        t0 = time.time()
-        interval_without_improvement = 0
-
-        while True:
-            try:
-                # evaluate the loss on train/val sets and write checkpoints
-                if self.iter_num % eval_interval == 0:
-                    losses = self.estimate_loss(eval_iters)
-                    # timing and logging
-                    eval_t1 = time.time()
-                    eval_dt = eval_t1 - eval_t0
-                    eval_t0 = eval_t1
-
-                    print(
-                        f"{Fore.WHITE}{Style.BRIGHT}step",
-                        f"{Fore.BLACK}{Style.BRIGHT}[{self.iter_num}/{max_iters}]"
-                        f"{Fore.RESET}{Style.RESET_ALL}:",
-                        f"train loss {Fore.WHITE}{Style.BRIGHT}{losses["train"]:.4f}"
-                        f"{Fore.RESET}{Style.RESET_ALL},",
-                        f"val loss {Fore.WHITE}{Style.BRIGHT}{losses["val"]:.4f}"
-                        f"{Fore.RESET}{Style.RESET_ALL},",
-                        f"time took {Fore.BLACK}{Style.BRIGHT}{calc_total_time(eval_dt)}"
-                    )
-
-                    self.metrics["train"].append(losses["train"])
-                    self.metrics["val"].append(losses["val"])
-
-                    # check for early stopping
-                    if losses["val"] < self.best_loss:
-                        self.best_loss = losses["val"]
-                        interval_without_improvement = 0  # reset the count
-
-                    elif interval_without_improvement + 1 >= patience:
-                        print("Early stopping due to no improvement in validation loss.")
-                        break
-
-                    else:
-                        interval_without_improvement += 1
-
-                if self.config["checkpoints"] and self.iter_num % self.config["checkpoints"]["interval"] == 0:
-                    if not os.path.isdir(self.config["checkpoints"]["path"]):
-                        os.mkdir(self.config["checkpoints"]["path"])
-
-                    if self.iter_num > 0:
-                        torch.save(self.get_trained_model(), f"{self.config["checkpoints"]["path"]}\\{self.config["checkpoints"]["name"]}_step{self.iter_num}.pth")
-
-                # sample a batch of data
-                X, Y = self.get_batch("train")
-
-                # evaluate the loss
-                _, loss = self.model(X, Y)
-                optimizer.zero_grad(set_to_none=True)
-                loss.backward()
-                optimizer.step()
-
-                # timing and logging
-                if self.iter_num % log_interval == 0:
-                    t1 = time.time()
-                    dt = t1 - t0
-                    t0 = t1
-
-                    # get loss as float. note: this is a CPU-GPU sync point
-                    lossf = loss.item()
-
-                    print(
-                        f"{Fore.WHITE}{Style.BRIGHT}iter",
-                        f"{Fore.BLACK}{Style.BRIGHT}[{self.iter_num}/{max_iters}]"
-                        f"{Fore.RESET}{Style.RESET_ALL}:",
-                        f"loss {Fore.WHITE}{Style.BRIGHT}{lossf:.4f}"
-                        f"{Fore.RESET}{Style.RESET_ALL},",
-                        f"{Fore.RESET}{Style.RESET_ALL},",
-                        f"time took {Fore.BLACK}{Style.BRIGHT}{calc_total_time(dt)}"
-                    )
-                    self.metrics["eval"].append(lossf)
-
-                self.iter_num += 1
-
-                # termination conditions
-                if self.iter_num > max_iters:
-                    break
-
-            except KeyboardInterrupt:
-                print(f"{Fore.RED}{Style.BRIGHT}Early stopping.")
-                break
-
-        print("Total time:", calc_total_time(time.time() - start_time))
-        return self.get_trained_model()
-
-    def get_trained_model(self):
-        return {
-            "model": self.model.state_dict(),
-            "hyperparams": self.hyperparams,
-            "iter_num": self.iter_num,
-            "device": self.device,
-            "metrics": self.metrics,
-            "best_loss": self.best_loss
-        }
---
+++ train_rnn.py
@@ -0,0 +1,175 @@
+from src.shared.utils import calc_total_time
+from src.models.rnn import RNNConfig, RNN
+from colorama import Style, Fore, init
+import torch._inductor.config as config
+import pickle, random, torch, json, time, os
+
+init(autoreset=True)
+
+with open("res\\config.json", "r", encoding="utf-8") as f:
+	CONFIG = json.load(f)["RNN"]
+
+device = ("cuda" if torch.cuda.is_available() else "cpu") if CONFIG["device"] == "auto" else CONFIG["device"]
+
+# init seed
+torch.manual_seed(CONFIG["seed"]) if CONFIG["seed"] != "auto" else None
+random.seed(CONFIG["seed"]) if CONFIG["seed"] != "auto" else None
+
+# print the device
+print("Training on", f"{Fore.YELLOW}{Style.BRIGHT}{device}", f"{Fore.WHITE}{Style.BRIGHT}({torch.initial_seed()})")
+
+hyperparams = dict(dropout=CONFIG["dropout"])
+# read off the created config params, so we can store them into checkpoint correctly
+for k in ["input_size", "output_size", "n_layer", "n_hidden", "n_embd", "dropout"]:
+	hyperparams[k] = CONFIG[k]
+
+rnnconf = RNNConfig(**hyperparams)
+# create an instance of RNN
+model = RNN(rnnconf)
+model.to(device)
+
+# optimizer
+optimizer = model.configure_optimizers(CONFIG["learning_rate"], CONFIG["device"])
+
+# a dict for keep track of all the losses to be plotted.
+metrics = {
+	"train": [],
+	"eval": [],
+	"val": []
+}
+iter_num = 0
+best_loss = 0
+
+def get_trained_model(model, optimizer):
+	return {
+		"model": model.state_dict(),
+		"optimizer": optimizer.state_dict(),
+		"hyperparams": hyperparams,
+		"device": device,
+		"iter_num": iter_num,
+		"best_loss": best_loss
+	}
+
+def _load_data(path):
+	if not CONFIG["load_from_file"]:
+		files = os.listdir(path)
+		random.shuffle(files)
+
+	with open(f"{path}\\{files[0]}" if not CONFIG["load_from_file"] else path, "rb") as f:
+		return pickle.load(f)
+
+# data loading
+# generate a small batch of data of inputs x and targets y
+def get_batch(split):
+	# we reload data every batch to avoid a memory leak
+	path = CONFIG["train_data"] if split == "train" else CONFIG["val_data"]
+	data = _load_data(path)
+
+	ix = torch.randint(len(data) - 1, (config["batch_size"],))
+	x = torch.stack([data[i][0] for i in ix])
+	y = torch.stack([data[i][1] for i in ix])
+	x, y = x.to(device), y.to(device)
+	return x, y
+
+# helps estimate an arbitrarily accurate loss over either split using many batches
+@torch.no_grad()
+def estimate_loss(eval_iters):
+	out = []
+	model.eval()
+	for split in ["train", "val"]:
+		losses = torch.zeros(eval_iters)
+		for k in range(eval_iters):
+			X, Y = get_batch(split)
+			logits, loss = model(X, Y)
+
+			losses[k] = loss.item()
+		out[split] = losses.mean()
+	model.train()
+	return out
+
+# report number of parameters
+print(f"{Fore.WHITE}{Style.BRIGHT}{model.get_num_params()/1e6}M", "parameters")
+
+# compile the model
+if CONFIG["compile"]:
+	print(f"Compiling the model... {Fore.BLACK}{Style.BRIGHT}(takes a ~minute)")
+	#NOTE: backend="inductor" is giving some errors so switched to aot_eager.
+	model = torch.compile(model, backend="aot_eager") # requires PyTorch 2.0
+
+# training loop
+start_time = time.time()
+eval_t0 = time.time()
+t0 = time.time()
+
+while True:
+	try:
+		# evaluate the loss on train/val sets and write checkpoints
+		if iter_num % CONFIG["eval_interval"] == 0:
+			losses = estimate_loss(CONFIG["eval_iters"])
+			# timing and logging
+			eval_t1 = time.time()
+			eval_dt = eval_t1 - eval_t0
+			eval_t0 = eval_t1
+
+			print(
+				f"{Fore.WHITE}{Style.BRIGHT}step",
+				f"{Fore.BLACK}{Style.BRIGHT}[{iter_num}/{CONFIG["max_iters"]}]"
+				f"{Fore.RESET}{Style.RESET_ALL}:",
+				f"train loss {Fore.WHITE}{Style.BRIGHT}{losses["train"]:.4f}"
+				f"{Fore.RESET}{Style.RESET_ALL},",
+				f"val loss {Fore.WHITE}{Style.BRIGHT}{losses["val"]:.4f}"
+				f"{Fore.RESET}{Style.RESET_ALL},",
+				f"time took {Fore.BLACK}{Style.BRIGHT}{calc_total_time(eval_dt)}"
+			)
+
+			metrics["train"].append(losses["train"])
+			metrics["val"].append(losses["val"])
+
+		if config["checkpoints"] and iter_num % config["checkpoints"]["interval"] == 0:
+			if not os.path.isdir(config["checkpoints"]["path"]):
+				os.mkdir(config["checkpoints"]["path"])
+
+			if iter_num > 0:
+				torch.save(get_trained_model(), f"{config["checkpoints"]["path"]}\\{config["checkpoints"]["name"]}_step{iter_num}.pth")
+
+		# sample a batch of data
+		X, Y = get_batch("train")
+
+		# evaluate the loss
+		_, loss = model(X, Y)
+		optimizer.zero_grad(set_to_none=True)
+		loss.backward()
+		optimizer.step()
+
+		# timing and logging
+		if iter_num % CONFIG["log_interval"] == 0:
+			t1 = time.time()
+			dt = t1 - t0
+			t0 = t1
+
+			# get loss as float. note: this is a CPU-GPU sync point
+			lossf = loss.item()
+
+			print(
+				f"{Fore.WHITE}{Style.BRIGHT}iter",
+				f"{Fore.BLACK}{Style.BRIGHT}[{iter_num}/{CONFIG["max_iters"]}]"
+				f"{Fore.RESET}{Style.RESET_ALL}:",
+				f"loss {Fore.WHITE}{Style.BRIGHT}{lossf:.4f}"
+				f"{Fore.RESET}{Style.RESET_ALL},",
+				f"{Fore.RESET}{Style.RESET_ALL},",
+				f"time took {Fore.BLACK}{Style.BRIGHT}{calc_total_time(dt)}"
+			)
+			metrics["eval"].append(lossf)
+
+		iter_num += 1
+
+		# termination conditions
+		if iter_num > CONFIG["max_iters"]:
+			break
+
+	except KeyboardInterrupt:
+		print(f"{Fore.RED}{Style.BRIGHT}Early stopping.")
+		break
+
+print("Total time:", calc_total_time(time.time() - start_time))
+torch.save(get_trained_model(model, optimizer), CONFIG["save_path"])
