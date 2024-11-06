c886436efc632e8df061b8e6676d6e82cc4b0537
SrijanSriv211 <QCoreNest@gmail.com>
2024-11-06 21:25:09 +0530

    Suppress future warnings in PyTorch

--- scripts/test_rnn.py
+++ scripts/test_rnn.py
@@ -5,7 +5,18 @@ from src.shared.nltk_utils import one_hot_encoding
 from src.models.encoder import Encoder
 from colorama import Style, Fore, init
 from src.models.rnn import sample
-import pickle, torch
+import warnings, pickle, torch
+
+# supress pytorch's future warning:
+# You are using `torch.load` with `weights_only=False` (the current default value), which uses the default pickle module implicitly.
+# It is possible to construct malicious pickle data which will execute arbitrary code during unpickling
+# (See https://github.com/pytorch/pytorch/blob/main/SECURITY.md#untrusted-models for more details).
+# In a future release, the default value for `weights_only` will be flipped to `True`.
+# This limits the functions that could be executed during unpickling.
+# Arbitrary objects will no longer be allowed to be loaded via this mode unless they are explicitly allowlisted by the user via `torch.serialization.add_safe_globals`.
+# We recommend you start setting `weights_only=True` for any use case where you don't have full control of the loaded file.
+# Please open an issue on GitHub for any issues related to this experimental feature.
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 init(autoreset=True)
 
