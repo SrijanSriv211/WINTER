fb843dddefa51bc2dfac991c73bfef00bcbcbb84
SrijanSriv211 <QCoreNest@gmail.com>
2024-11-16 15:23:08 +0530

    Update prepare_gpt.py

--- scripts/prepare_gpt.py
+++ scripts/prepare_gpt.py
@@ -3,6 +3,7 @@ sys.path.insert(0, "D:\\Dev Projects\\WINTER")
 
 from src.models.encoder import Encoder
 from src.shared.utils import prepare_data
+import os
 
 enc = Encoder()
 enc.load("bin\\cl8k.bin")
@@ -10,23 +11,12 @@ enc.load("bin\\cl8k.bin")
 def get_foundational_dataset():
 	data = []
 
-	with open("data\\claw\\raw\\Knowledge copy.txt", "r", encoding="utf-8") as f:
-		data.append(f.read())
+	files = os.listdir("data\\claw\\raw")
+	files.remove("claw.txt")
 
-	with open("data\\claw\\raw\\General notes.txt", "r", encoding="utf-8") as f:
-		data.append(f.read())
-
-	with open("data\\claw\\raw\\jokes.txt", "r", encoding="utf-8") as f:
-		data.append(f.read())
-
-	with open("data\\claw\\raw\\LLM data.txt", "r", encoding="utf-8") as f:
-		data.append(f.read())
-
-	with open("data\\claw\\raw\\facts.txt", "r", encoding="utf-8") as f:
-		data.append(f.read())
-
-	with open("data\\claw\\raw\\wikitext.txt", "r", encoding="utf-8") as f:
-		data.append(f.read())
+	for i in files:
+		with open(f"data\\claw\\raw\\{i}", "r", encoding="utf-8") as f:
+			data.append(f.read()[:200_000_000] if i == "gpt-2.txt" else f.read())
 
 	data = "\n\n".join(data)
 	print(f"{(len(data)/1e6)}M total chars", f"{(len(set(data)))} unique chars")
